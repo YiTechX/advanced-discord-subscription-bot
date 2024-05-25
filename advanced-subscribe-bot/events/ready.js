a2c8501d33c5c6187dcf7e926a47790aaaa5190b
Ysara Frostwind <158102730+YiTechX@users.noreply.github.com>
2024-05-25 10:57:21 +0300

    Update ready.js

---
+++ advanced-subscribe-bot/events/ready.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute(client) {
+        console.log(`Logged in as ${client.user.tag}!`);
+
+        
+        client.user.setPresence({
+            activities: [{ name: 'discord.gg/marvelcode', type: 'WATCHING' }],
+            status: 'online',
+        });
+
+        console.log(`Bot status set to "WATCHING YouTube subscriptions"`);
+    },
+};
